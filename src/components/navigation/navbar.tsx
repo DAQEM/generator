ee32e7ed87c014db6aa5c23310b869f680c1ec2c
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-01-14 23:51:58 +0100

    feat: add reset button to navbar

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,11 +1,23 @@
 import logo from "@/assets/logo.svg";
+import { useStore } from "@/store/store";
 import { Button } from "../ui/button";
 
 const Navbar = () => {
+    const { selectProject, selectJob } = useStore();
+
     return (
         <header className="h-20 w-full px-4 py-2 bg-white">
             <div className="px-4 flex items-center justify-between h-full w-full">
-                <img src={logo} alt="Logo" height="56px" width="130px" />
+                <Button
+                    variant="ghost"
+                    className="w-32.5 h-14 p-0"
+                    onClick={() => {
+                        selectProject(null);
+                        selectJob(null);
+                    }}
+                >
+                    <img src={logo} alt="Logo" height="56px" width="130px" />
+                </Button>
                 <Button asChild>
                     <a
                         href="https://daqem.com/discord"
